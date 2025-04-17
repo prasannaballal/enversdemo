9f0eb8db11dff31ff7978d53024dc3828b60c199
Prasanna V Ballal <24prasannaballal@gmail.com>
2025-04-17 15:51:00 +0200

    Update EnversController.java

--- src/main/java/com/example/envers/enversdemo/EnversController.java
+++ src/main/java/com/example/envers/enversdemo/EnversController.java
@@ -1,1 +1,27 @@
+package com.example.envers.enversdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EnversController {
+
+    @Autowired
+    EnversService enversService;
+
+    @PostMapping("/envers")
+    public ResponseEntity<String> createPerson(@RequestBody PersonDto personDto) {
+        return ResponseEntity.ok(enversService.createPerson(personDto));
+    }
+
+    @PutMapping("/envers")
+    public ResponseEntity<String> updatePerson(@RequestBody PersonDto personDto) {
+        return ResponseEntity.ok(enversService.updatePerson(personDto));
+    }
+
+    @DeleteMapping("/envers/{id}")
+    public ResponseEntity<String> deletePerson(@PathVariable Long id) {
+        return ResponseEntity.ok(enversService.deletePerson(id));
+    }
+}

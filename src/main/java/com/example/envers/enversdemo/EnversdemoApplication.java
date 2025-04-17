81005fef8f09dd4589332b36c20f0eb6c7cabd9d
Prasanna V Ballal <24prasannaballal@gmail.com>
2025-04-17 15:50:43 +0200

    Create EnversdemoApplication.java

---
+++ src/main/java/com/example/envers/enversdemo/EnversdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.envers.enversdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnversdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnversdemoApplication.class, args);
+	}
+
+}

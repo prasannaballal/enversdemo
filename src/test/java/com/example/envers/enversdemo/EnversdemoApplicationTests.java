071e40582d7e2a18a5e0417b71a7731975251658
Prasanna V Ballal <24prasannaballal@gmail.com>
2025-04-17 15:57:31 +0200

    Create EnversdemoApplicationTests.java

---
+++ src/test/java/com/example/envers/enversdemo/EnversdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.envers.enversdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnversdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c88bbed61aef7da92b617db8a78fbd934909e3f8
Prasanna V Ballal <24prasannaballal@gmail.com>
2025-04-17 15:52:20 +0200

    Create PersonDto.java

---
+++ src/main/java/com/example/envers/enversdemo/PersonDto.java
@@ -0,0 +1,38 @@
+package com.example.envers.enversdemo;
+
+public class PersonDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
